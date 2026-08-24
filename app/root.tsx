a897bc66264e13100bcc3df01a1cedc508efdd53
lorenjordan333-dev <lorenjordan333@gmail.com>
2026-08-24 19:53:24 +0200

    Add the DebuggerOS widget

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <script src="https://debuggeros.com/debuggeros-widget.js" data-project-id="b5924ffa-0595-40d4-81e8-a8eeb7a57e5b" data-endpoint="https://debuggeros.com" defer></script>
       </body>
     </html>
   );
